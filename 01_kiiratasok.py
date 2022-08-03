5c1424b022fc72b69b11d6ecdea79bd49a376912
Ákos Klobetz <klobetz@gmail.com>
2022-08-03 09:52:06 +0200

    első óra

---
+++ 01_kiiratasok.py
@@ -0,0 +1,14 @@
+#képernyőre kiíratás
+print("Első programom!")            #futtatáshoz shift+ctrl+F10
+print('Első programom!')            #vagy így
+print('Első programom! "idézet" ')
+#print("Első programom! "idézet" ")            #ez íg hibás kiíratás SyntaxError:
+
+#több sorbn való kiíratás?
+print('második '                 #ez így egy sorban írja ki a kiíratást
+      'programom! de nem jó')
+#ezt használjuk több sorhoz!
+print("""első sor
+valami
+valami kettő
+""")
