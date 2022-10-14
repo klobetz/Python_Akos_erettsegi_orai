636bcfb2a39a87f0c19b09b5c7c65d3c982f70d8
Ákos Klobetz <klobetz@gmail.com>
2022-10-14 16:30:18 +0200

    10. óra

--- 06_sztringek.py
+++ 06_sztringek.py
@@ -27,3 +27,41 @@ print(sztring[:5])
 print(sztring[5:])
 print(sztring[:5])
 print(sztring[7:])
+
+#karakterek vizsgálata in és a not operátorok:
+# in operátor:
+print("a" in sztring)
+
+if "a" in sztring:
+    print("szerepel")
+else:
+    print("nem szerepel")
+
+#not opreátor:
+print("a" not in sztring)
+if "a" not in sztring:
+    print("nem szerepel")
+else:
+    print("szerepel")
+
+#beépített find metodus
+
+varos = "Körmend"
+
+print(varos.find("ö"))
+print(type(varos.find("ö")))
+print(f"A keresett karakter a/az {varos.find('ö')+1} helyen szerepel.")
+#ha nincs benne akkor (-1)
+print(varos.find("á"))
+
+mandat = "Én már kezdem érteni az adott tananyagot!"
+print(mandat.split())
+lista = mandat.split()
+print(type(lista))
+
+for elem in lista:
+    print(elem, end=" ")
+
+print()
+print(len(mandat.split()))
+
